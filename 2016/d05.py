6e746e8dc97108158d76616e85234ff2f1a5389d
Nathaniel Grill <nathaniel.grill@gmail.com>
2021-06-15 12:35:47 -0500

    added cinematic "decrypting" animation

--- 2016/d05.py
+++ 2016/d05.py
@@ -1,21 +1,29 @@
 '''
-AoC 2015 Day 4 parts 1 & 2
+AoC 2016 Day 5 parts 1 & 2
 '''
 
 from hashlib import md5
 
 def main():
-    key, value, num, five_found = 'iwrupvqb', '', 0, False
+    key, num = 'ffykfhsq', 0
+    pass_easy, pass_hard = '', ['-','-','-','-','-','-','-','-']
 
-    while value[:6] != '0'*6:
+    print('Password:', ''.join(p for p in pass_hard))
+    while '-' in pass_hard:
         num += 1
         value = md5((key + str(num)).encode()).hexdigest()
 
-        if value[:5] == '00000' and not five_found:
-            print('Five zero hash:', num)
-            five_found = True
+        if value[:5] == '00000':
+            v = value[5]
+            if len(pass_easy) < 8: pass_easy += v
+            try:
+                v = int(v)
+                if v < 8 and pass_hard[v] == '-':
+                    pass_hard[v] = value[6]
+                    print('Password:', ''.join(p for p in pass_hard))
+            except ValueError: continue
 
-    print('Six zero hash:', num)
+    print('Easy:', pass_easy, '\nHard:', ''.join(p for p in pass_hard))
 
 if __name__ == "__main__":
     main()
